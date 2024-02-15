a04a9b60ff5541ba3d955444f69d1a5aa7b63944
shashidhara-ruchika <shashidhara.r@northeastern.edu>
2024-02-15 15:39:12 -0500

    create multiple vpc

--- network.tf
+++ network.tf
@@ -1,117 +1,75 @@
 provider "google" {
-  credentials = file(var.credentials_path)
+  credentials = file(var.credentials_file_path)
   project     = var.project_id
   region      = var.region
 }
 
+# Loop through the list of VPC configurations and create VPCs
 resource "google_compute_network" "vpc" {
-  name                    = var.vpc_name
-  auto_create_subnetworks = var.vpc_auto_create_subnetworks
-  routing_mode            = var.vpc_routing_mode
-  delete_default_routes_on_create = var.vpc_delete_default_routes_on_create
+  count                           = length(var.vpcs)
+  name                            = var.vpcs[count.index].vpc_name
+  auto_create_subnetworks         = var.vpcs[count.index].vpc_auto_create_subnetworks
+  routing_mode                    = var.vpcs[count.index].vpc_routing_mode
+  delete_default_routes_on_create = var.vpcs[count.index].vpc_delete_default_routes_on_create
 }
 
 resource "google_compute_subnetwork" "webapp" {
-  name          = var.vpc_subnet_webapp_name
-  ip_cidr_range = var.vpc_subnet_webapp_ip_cidr_range
-  network       = google_compute_network.vpc.self_link
+  count         = length(var.vpcs)
+  name          = var.vpcs[count.index].vpc_subnet_webapp_name
+  ip_cidr_range = var.vpcs[count.index].vpc_subnet_webapp_ip_cidr_range
+  network       = google_compute_network.vpc[count.index].self_link
+  region        = var.region
 }
 
 resource "google_compute_subnetwork" "db" {
-  name          = var.vpc_subnet_db_name
-  ip_cidr_range = var.vpc_subnet_db_ip_cidr_range
-  network       = google_compute_network.vpc.self_link
+  count         = length(var.vpcs)
+  name          = var.vpcs[count.index].vpc_subnet_db_name
+  ip_cidr_range = var.vpcs[count.index].vpc_subnet_db_ip_cidr_range
+  network       = google_compute_network.vpc[count.index].self_link
+  region        = var.region
 }
 
 resource "google_compute_route" "webapp_route" {
-  name         = var.vpc_route_webapp_route_name
-  network      = google_compute_network.vpc.self_link
-  dest_range   = var.vpc_route_webapp_route_range
-  next_hop_gateway = var.vpc_route_webapp_route_next_hop_gateway
-  priority     = var.vpc_route_webapp_route_priority
+  count            = length(var.vpcs)
+  name             = "webapp-route-${count.index}"
+  network          = google_compute_network.vpc[count.index].self_link
+  dest_range       = var.vpcs[count.index].vpc_dest_range
+  next_hop_gateway = var.vpcs[count.index].vpc_next_hop_gateway
+  priority         = var.vpc_route_webapp_route_priority
 }
 
+# Define a variable to store VPC configurations
+variable "vpcs" {
+  type = list(object({
+    vpc_name                            = string
+    vpc_subnet_webapp_name              = string
+    vpc_subnet_webapp_ip_cidr_range     = string
+    vpc_subnet_db_name                  = string
+    vpc_subnet_db_ip_cidr_range         = string
+    vpc_routing_mode                    = string
+    vpc_dest_range                      = string
+    vpc_auto_create_subnetworks         = bool
+    vpc_delete_default_routes_on_create = bool
+    vpc_next_hop_gateway                = string
+  }))
+}
 
-
-variable "credentials_path" {
-  description = "The path to the service account key file."
-  type = string
+variable "credentials_file_path" {
+  description = "service-account-key.json"
+  type        = string
 }
 
 variable "project_id" {
-  description = "The ID of the Google Cloud Platform project."
-  type = string
+  description = "Project ID"
+  type        = string
 }
 
 variable "region" {
-  description = "The region where resources will be deployed."
-  type = string
-}
-
-
-
-variable "vpc_name" {
-  description = "The name of the Virtual Private Cloud (VPC)."
-  type = string
-}
-
-variable "vpc_auto_create_subnetworks" {
-  description = "Whether to auto-create subnetworks in the VPC."
-  type        = bool
-}
-
-variable "vpc_routing_mode" {
-  description = "The routing mode for the VPC."
-  type = string
-}
-
-variable "vpc_delete_default_routes_on_create" {
-  description = "Whether to delete default routes when creating the VPC."
-  type        = bool
-}
-
-
-
-variable "vpc_subnet_webapp_name" {
-  description = "The name of the subnet for the web application."
-  type = string
-}
-
-variable "vpc_subnet_webapp_ip_cidr_range" {
-  description = "The IP CIDR range for the web application subnet."
-  type = string
-}
-
-
-
-variable "vpc_subnet_db_name" {
-  description = "The name of the subnet for the database."
-  type = string
-}
-
-variable "vpc_subnet_db_ip_cidr_range" {
-  description = "The IP CIDR range for the database subnet."
-  type = string
-}
-
-
-
-variable "vpc_route_webapp_route_name" {
-  description = "The name of the route for the web application subnet."
-  type = string
-}
-
-variable "vpc_route_webapp_route_range" {
-  description = "The destination range for the web application route."
-  type = string
-}
-
-variable "vpc_route_webapp_route_next_hop_gateway" {
-  description = "The next hop gateway for the web application route."
-  type = string
+  description = "GCP Region"
+  type        = string
 }
 
 variable "vpc_route_webapp_route_priority" {
-  description = "The priority for the web application route."
+  description = "priority"
   type        = number
 }
